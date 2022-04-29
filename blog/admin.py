7618380c40b774d41ff37afb12d6ce7aeabd00ca
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-04-29 21:07:25 +0200

    Add Admin Post class and Comment Admin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    # prepopulated_fields is a django special propery designed for slug fields
+    # uses JS to format and displat slug field
+    prepopulated_fields = {'slug': ('title',)}
+    #  filter box in the post lists, passing what options to use for filtering
+    list_filter = ('status', 'created_on')
+    # how posts are displayed in the list
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # adds search field which searches posts by arguments passed
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    # allows to specify different actions from the action drop-down box
+    # default action is delete the selected item
+    # accepts functions as arguments, so must build one to use it
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        # method updates the queryset approved to True (action call)
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
+import email
+from email.policy import default
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.forms import BooleanField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # related_name attribiute specifies the name of the reverse relation from the User model back to your model
+    # if it is not specified, django creates one using name of model with added _set
+    # e.x. User.post_set, or User.blog_posts if specified
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    class Meta:
+        # all post will be order base on created_on attribiute
+        #  '-' signs means it will be descending order
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
