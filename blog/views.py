1f1c7422c86529ad12446ec42f12ae0c440b3787
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-05-01 10:55:33 +0200

    Add PostDetail view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import imp
 from re import template
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.views import generic, View
 from .models import Post
 
 class PostList(generic.ListView):
@@ -11,3 +11,26 @@ class PostList(generic.ListView):
     # limits number of post visibile on page to 6
     # if more than 6, django will add page navigation
     paginate_by = 6
+
+
+class PostDetail(View):
+    # in class based views we do operations with POST and GET by defining methods
+    def get(self, request, slug, *args, **kwargs):
+        # filtering post which are finished (not draft)
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        # checking if user liked the post and setting appropriate boolean
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked":liked
+            },
+        )
