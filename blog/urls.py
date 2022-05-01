3285b5f8731808dc77fb35f226cf34d5ddbeb2ee
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-05-01 18:14:18 +0200

    Edit post_detail template and wire up url

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     # empty string for default home page
     # .as_view() method must be add if using class based views
     path('', views.PostList.as_view(), name='home'),
+    # first slug is path converter, second is keyword name from views.py
+    # pathconverter converts keyword into the slug field
+    # it's built-in django feature, it tells django to match any slug string, which consists of ASCII characters...
+    # ... numbers plus hyphen and underscore characters
+    path('<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
 ]
