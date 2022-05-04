ad3712e58b4663895d1cc324c3080a9bad0021a7
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-05-04 19:56:58 +0200

    Add comments and likes functionality

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     # it's built-in django feature, it tells django to match any slug string, which consists of ASCII characters...
     # ... numbers plus hyphen and underscore characters
     path('<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.shortcuts import get_object_or_404, render, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -31,7 +32,60 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
                 "liked":liked,
                 "comment_form": CommentForm(),
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        # filtering post which are finished (not draft)
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        # checking if user liked the post and setting appropriate boolean
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        # this will get all the data posted in the form
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            # saving comment_form with passed data to variable
+            # no commit is passed to first connect it to the post instance
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            # if user is not authenticated the empty comment form will be passed
+            comment_form = CommentForm()
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked":liked,
+                "comment_form": CommentForm(),
+            },
+        )
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        # new response type to reload post_detail view
+        # reverse shortcut gives ability to look up for url name(given in urls.py)
+        # args is what comes in url, so slug will be pasted '.../<slug>/'
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
